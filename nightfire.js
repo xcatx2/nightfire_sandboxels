b3b5754205ae35fec897099dae62c19dc9757db9
xcatx2 <xcatx02@gmail.com>
2025-08-12 21:16:01 -0700

    Update nightfire.js

--- nightfire.js
+++ nightfire.js
@@ -1,6 +1,6 @@
 elements.nightfire = {
-  behaviour: behaviours.GAS,
+  behavior: behaviors.GAS,
   category: "gases", 
   state: "gas", 
-  colour: ["#404040", "#545454"]
+  color: ["#404040", "#545454"]
 };
